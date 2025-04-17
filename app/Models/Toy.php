4720b84ee2f75d2a55ef580733732485616f3cdb
JazzAnt <jason.kwee98@gmail.com>
2025-04-17 01:41:37 -0700

    create toy model and migration table

---
+++ app/Models/Toy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Toy extends Model
+{
+    use HasFactory;
+
+    public function getId(){
+        return $this->attributes['id'];
+    }
+    public function setId(){
+        return $this->attributes['id'];
+    }
+    public function getName(){
+        return $this->attributes['name'];
+    }
+    public function setName(){
+        return $this->attributes['name'];
+    }
+    public function getDescription(){
+        return $this->attributes['description'];
+    }
+    public function setDescription(){
+        return $this->attributes['description'];
+    }
+    public function getImage(){
+        return $this->attributes['image'];
+    }
+    public function setImage(){
+        return $this->attributes['image'];
+    }
+    public function getPrice(){
+        return $this->attributes['price'];
+    }
+    public function setPrice(){
+        return $this->attributes['price'];
+    }
+    public function getQuantity(){
+        return $this->attributes['quantity'];
+    }
+    public function setQuantity(){
+        return $this->attributes['quantity'];
+    }
+    public function getTimestamp(){
+        return $this->attributes['timestamp'];
+    }
+    public function setTimestamp(){
+        return $this->attributes['timestamp'];
+    }
+}
