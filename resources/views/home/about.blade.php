a628b6f6b463d503aeab443b79e6f139ab3a0601
JazzAnt <jason.kwee98@gmail.com>
2025-04-17 15:22:37 -0700

    add about page

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -2,5 +2,18 @@
 @section('title', $viewdata["title"])
 @section('subtitle', $viewdata["subtitle"])
 @section('content')
-
+<div class="card">
+<div class="row">
+<div class="col-lg-8">
+    <h2 class="card-title">About Us</h2>
+    <ul>
+        <li class="card-text">Name: Jason Januar</li>
+        <li class="card-text">ID: 300393865</li>
+        <li class="card-text">Class: CSIS 3280</li>
+        <li class="card-text">Semester: Winter 2025</li>
+        <li class="card-text">Instructor: Bambang Sarif</li>
+    </ul>
+</div>
+</div>
+</div>
 @endsection
