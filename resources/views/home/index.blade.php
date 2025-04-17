be571aef1df6b8b61ca2b10355744f10cc4b5f45
JazzAnt <jason.kwee98@gmail.com>
2025-04-17 15:18:44 -0700

    add home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,5 +2,23 @@
 @section('title', $viewdata["title"])
 @section('subtitle', $viewdata["subtitle"])
 @section('content')
-
+<div class="card">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="card-title">Welcome to the Toy Store!</h2>
+        </div>
+        <div class="col-md-8">
+            <p class="card-text">
+                Welcome to our toy store. Here we mainly sell Remote Control (RC) Cars but you can find other toys as well! 
+                We hope you enjoy your visit to our website and spend lots of money!
+            </p>
+        </div>
+        <div class="col-md-4">
+            <figure>
+                <img src="{{ asset('/img/rc-cars.jpg') }}">
+                <figcaption>Our RC Cars in a Race!</figcaption>
+            </figure>
+        </div>
+    </div>
+</div>
 @endsection
