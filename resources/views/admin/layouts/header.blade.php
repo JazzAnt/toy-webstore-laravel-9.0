80e1d9926115452bfa82374e6303e53c333cc305
JazzAnt <jason.kwee98@gmail.com>
2025-04-19 18:18:28 -0700

    modify admin header

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,18 +1,13 @@
 @section('header')
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
-    <div class="container">
-        <a class="navbar-brand" href="{{ route('admin.home.index') }}">Toy Webstore Administration</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-                <a class="nav-link active" href="{{ route('admin.home.index') }}">Admin</a>
-                <a class="nav-link active" href="{{ route('admin.toy.index') }}">Catalog Management</a>
-                <a class="nav-link active" href="{{ route('home.index') }}">Back to Home</a>
-            </div>
-        </div>
-    </div>
-</nav>
+<header>
+    <h1>@yield('subtitle', 'Toy Webstore Administration')</h1>
+    <nav id="navigation-bar">
+        <ul>
+            <li><a class="nav-link active" href="{{ route('admin.home.index') }}">Admin</a></li>
+            <li><a class="nav-link active" href="{{ route('admin.toy.index') }}">Catalog Management</a></li>
+            <li><a class="nav-link active" href="{{ route('home.index') }}">Back to Home</a></li>
+            <li><a class="nav-link active" href="{{ route('home.index') }}">Logout</a></li>
+        </ul>
+    </nav>
+</header>
 @endsection
